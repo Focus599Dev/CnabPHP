38a269a5868763ed53b59c4b8e816d38315aee5d
Willian Reis <willian.fagoc@gmail.com>
2017-08-18 18:05:13 -0300

    Implementacao santander-cnab240

--- src/Resources/Santander/retorno/L040/Registro1.php
+++ src/Resources/Santander/retorno/L040/Registro1.php
@@ -5,7 +5,7 @@
  * Date: 18/08/17
  * Time: 11:23
  */
-namespace CnabPHP\Resources\CaixaEconomicaFederal\retorno\L040;
+namespace CnabPHP\Resources\Santander\retorno\L040;
 use CnabPHP\Resources\Generico\retorno\L040\Generico1;
 use CnabPHP\RetornoAbstract;
 
--- src/Resources/Santander/retorno/L040/Registro3Y.php
+++ src/Resources/Santander/retorno/L040/Registro3Y.php
@@ -5,7 +5,7 @@
  * Date: 18/08/17
  * Time: 11:33
  */
-namespace CnabPHP\Resources\CaixaEconomicaFederal\retorno\L040;
+namespace CnabPHP\Resources\Santander\retorno\L040;
 use CnabPHP\Resources\Generico\retorno\L040\Generico3;
 use CnabPHP\RetornoAbstract;
 
