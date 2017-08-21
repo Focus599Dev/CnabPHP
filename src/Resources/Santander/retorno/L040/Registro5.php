cd41504a6c9a9c6c3fe4b9dd25e24eb97c206d5c
Willian Reis <willian.fagoc@gmail.com>
2017-08-21 14:07:28 -0300

    Implementacao santander-cnab240

--- src/Resources/Santander/retorno/L040/Registro5.php
+++ src/Resources/Santander/retorno/L040/Registro5.php
@@ -11,5 +11,74 @@ use CnabPHP\RetornoAbstract;
 
 class Registro5 extends Generico5
 {
-
+    protected $meta = array(
+        'codigo_banco'=>array(
+            'tamanho'=>3,
+            'default'=>'033',
+            'tipo'=>'int',
+            'required'=>true),
+        'codigo_lote'=>array(
+            'tamanho'=>4,
+            'default'=>'',
+            'tipo'=>'int',
+            'required'=>true),
+        'tipo_registro'=>array(
+            'tamanho'=>1,
+            'default'=>'5',
+            'tipo'=>'int',
+            'required'=>true),
+        'filler1'=>array(
+            'tamanho'=>9,
+            'default'=>' ',
+            'tipo'=>'alfa',
+            'required'=>true),
+        'qtd_registros'=>array(
+            'tamanho'=>6,
+            'default'=>'',
+            'tipo'=>'int',
+            'required'=>true),
+        'qtd_titulos_simples'=>array(
+            'tamanho'=>6,
+            'default'=>'0',
+            'tipo'=>'int',
+            'required'=>true),
+        'vrl_titulos_simples'=>array(
+            'tamanho'=>15,
+            'default'=>'0',
+            'tipo'=>'decimal',
+            'precision'=>'2',
+            'required'=>true),
+        'qtd_titulos_caucionada'=>array(
+            'tamanho'=>6,
+            'default'=>'0',
+            'tipo'=>'int',
+            'required'=>true),
+        'vlr_titulos_caucionada'=>array(
+            'tamanho'=>15,
+            'default'=>'0',
+            'tipo'=>'decimal',
+            'precision'=>'2',
+            'required'=>true),
+        'qtd_titulos_descontada'=>array(
+            'tamanho'=>6,
+            'default'=>'0',
+            'tipo'=>'int',
+            'required'=>true),
+        'vlr_titulos_descontada'=>array(
+            'tamanho'=>15,
+            'default'=>'0',
+            'tipo'=>'decimal',
+            'precision'=>'2',
+            'required'=>true),
+        'num_aviso_lancamento'=>array(
+            'tamanho'=>8,
+            'default'=>' ',
+            'tipo'=>'alfa',
+            'required'=>true),
+        'filler3'=>array(
+            'tamanho'=>117,
+            'default'=>' ',
+            'tipo'=>'alfa',
+            'required'=>true),
+    );
 }
