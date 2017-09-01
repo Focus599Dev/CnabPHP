3eb3947bc80f0fdb204e7418996b5809d7c4ff73
Willian Reis <willian.fagoc@gmail.com>
2017-09-01 13:48:42 -0300

    Implementacao santander-cnab240

--- src/Resources/B237/B033/retorno/L040/Registro3U.php
+++ src/Resources/B237/B033/retorno/L040/Registro3U.php
@@ -5,7 +5,7 @@
  * Date: 18/08/17
  * Time: 11:32
  */
-namespace CnabPHP\Resources\Santander\retorno\L040;
+namespace CnabPHP\Resources\B033\retorno\L040;
 use CnabPHP\Resources\Generico\retorno\L040\Generico3;
 use CnabPHP\RetornoAbstract;
 
